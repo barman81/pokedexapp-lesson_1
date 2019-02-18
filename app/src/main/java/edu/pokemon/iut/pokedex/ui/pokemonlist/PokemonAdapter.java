9fe14b5a4d78a8c594a165d2695635922ad4ff19
Maël Baron <mael.baron@laposte.net>
2019-02-18 13:21:57 +0100

    commit init

---
+++ app/src/main/java/edu/pokemon/iut/pokedex/ui/pokemonlist/PokemonAdapter.java
@@ -0,0 +1,98 @@
+package edu.pokemon.iut.pokedex.ui.pokemonlist;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v4.view.ViewCompat;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.pokemon.iut.pokedex.R;
+import edu.pokemon.iut.pokedex.architecture.NavigationManager;
+import edu.pokemon.iut.pokedex.data.model.Pokemon;
+
+/**
+ * Custom adapter to show each pokemon in a single line view
+ */
+public class PokemonAdapter extends RecyclerView.Adapter<PokemonAdapter.ViewHolder> {
+    private final Context context;
+    private final NavigationManager navigationManager;
+    private final CaptureListener captureListener;
+    private List<Pokemon> dataSet;
+
+    PokemonAdapter(Context context, NavigationManager navigationManager, CaptureListener captureListener) {
+        this.context = context;
+        this.navigationManager = navigationManager;
+        this.captureListener = captureListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.pokemon_line_view;
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(layoutIdForListItem, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        /* Mapping of the data into the view*/
+        Pokemon pokemon = dataSet.get(position);
+
+        // TODO 5) RECUPERER VIA LE HOLDER LA TEXTVIEW
+        TextView txvtiew = holder.pokemonName;
+
+        // TODO 6) INSERER LE NOM DU POKEMON DANS LA TEXTVIEW
+        txvtiew.setText(pokemon.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (dataSet != null) {
+            return dataSet.size();
+        } else {
+            return 0;
+        }
+    }
+
+    public void setData(List<Pokemon> data) {
+        this.dataSet = data;
+        notifyDataSetChanged();
+    }
+
+    public interface CaptureListener {
+        void onCapture(Pokemon pokemon);
+    }
+
+    /**
+     * Inner ViewHolder for the Pokemon's view
+     */
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        //TODO 3) DECLARER ICI UNE VARIABLE POUR LA TEXTVIEW
+        final TextView pokemonName;
+        final ImageView pokemonImage;
+
+        ViewHolder(View v) {
+            super(v);
+            //TODO 4) RECUPERER ICI UNE INSTANCE DE LA TEXTVIEW ET LA SAUVEGARGER DANS LA VARIABLE
+            pokemonName = v.findViewById(R.id.tv_pokemon_name);
+            pokemonImage = v.findViewById(R.id.iv_pokemon_image);
+
+        }
+    }
+
+}
+
